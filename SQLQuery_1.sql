8dadc34543d9c77cf169e412e2108cb0aa285798
Francesco Caputo <147523488+LouFranc0@users.noreply.github.com>
2024-02-07 15:04:42 +0100

    Add files via upload

---
+++ SQLQuery_1.sql
@@ -0,0 +1,50 @@
+--1
+
+SELECT COUNT (*) as TotalOrder from Orders;
+
+--2 
+
+SELECT COUNT (*) as TotalEmployees from Employees;
+
+--3 
+
+SELECT COUNT (*) as TotalClientsLiveinLondon from Customers where City='London';
+
+--4
+
+SELECT AVG(freight) as AvgOrders from Orders;
+
+--5
+
+SELECT avg (freight) from Orders where CustomerID='BOTTM';
+
+--6
+SELECT  avg (freight) from Orders group by CustomerID;
+
+--7
+
+SELECT count(*), City FROM Employees GROUP BY City;
+
+--8
+
+SELECT OrderID, sum(UnitPrice * Quantity) as Total from  [Order Details] GROUP BY OrderID;
+
+--9
+
+SELECT OrderID, sum(UnitPrice * Quantity) as Total from  [Order Details] GROUP BY OrderID HAVING OrderID=10248;
+
+--10
+
+SELECT count(*) as TotalProduct from Products group by CategoryID;
+
+--11 
+
+SELECT count(*) as TotalOrders from Orders group by ShipCountry;
+
+--12 
+
+SELECT AVG(Freight) as TrasportCosts from Orders;
+
+
+
+
